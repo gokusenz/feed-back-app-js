10820678de2bf685114c4672825e4793773be21a
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-22 13:17:09 +0700

    refactor

--- src/common/routes.js
+++ src/common/routes.js
@@ -12,7 +12,10 @@ const routes = (store, history) => (
   >
     <Route path="/">
       <IndexRedirect to="/404" />
-      <Route path="workshop/js-async-await" component={() => <MainApp name="js-async-await" topic="How to use Async/await (ES2017)" />} />
+      <Route
+        path="workshop/js-async-await" component={() =>
+          <MainApp name="js-async-await" topic="How to use Async/await (ES2017)" />}
+      />
       <Route path="report/:name" component={ReportContainer} />
       <Route path="404" component={PageNotFound} />
       <Redirect from="*" to="/404" />
