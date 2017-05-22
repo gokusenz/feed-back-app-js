fd525ddfce58433ef43acb51d0e9fed85131ce31
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-22 13:02:50 +0700

    fix routes

--- src/common/routes.js
+++ src/common/routes.js
@@ -3,6 +3,7 @@ import { Router, Route, IndexRedirect, Redirect } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import PageNotFound from './components/PageNotFound'
 import MainApp from './components/MainApp'
+import ReportContainer from './containers/Report'
 
 const routes = (store, history) => (
   <Router
@@ -12,6 +13,7 @@ const routes = (store, history) => (
     <Route path="/">
       <IndexRedirect to="/404" />
       <Route path="workshop/js-async-await" component={() => <MainApp name="js-async-await" topic="How to use Async/await (ES2017)" />} />
+      <Route path="report/:name" component={ReportContainer} />
       <Route path="404" component={PageNotFound} />
       <Redirect from="*" to="/404" />
     </Route>
